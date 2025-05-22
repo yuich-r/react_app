4fcda4399af96ee21ec6721d147b053d12c42beb
yuich-r <tukushi0829@icloud.com>
2025-05-22 13:45:13 +0900

    TOPへ戻るボタンを修正
    TOPを戻るボタンの上下を反転

--- src/components/ScrollToTopButton.js
+++ src/components/ScrollToTopButton.js
@@ -32,8 +32,7 @@ function ScrollToTopButton() {
   return (
     <div className={`scroll-to-top ${isVisible ? 'visible' : ''}`} onClick={scrollToTop}>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-        <path fillRule="evenodd" d="M12 3.75a.75.75 0 01.75.75v10.638l5.47-5.47a.75.75 0 111.06 1.06l-6.75 6.75a.75.75 0 01-1.06 0l-6.75-6.75a.75.75 0 111.06-1.06l5.47 5.47V4.5a.75.75 0 01.75-.75z" clipRule="evenodd" />
-      </svg>
+        <path fillRule="evenodd" d="M12 14.25a.75.75 0 01-.75-.75V2.862l-5.47 5.47a.75.75 0 11-1.06-1.06l6.75-6.75a.75.75 0 011.06 0l6.75 6.75a.75.75 0 11-1.06 1.06l-5.47-5.47V13.5a.75.75 0 01-.75.75z" clipRule="evenodd" />      </svg>
     </div>
   );
 }
