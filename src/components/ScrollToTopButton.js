635ea4cd914ddd8f6ef3c7271a0092e02fd2f631
yuich-r <tukushi0829@icloud.com>
2025-05-24 18:17:43 +0900

    TOPへ戻るボタンを修正
    TOPへ戻るボタンの矢印の位置が高かったため中央に修正

--- src/components/ScrollToTopButton.js
+++ src/components/ScrollToTopButton.js
@@ -32,7 +32,7 @@ function ScrollToTopButton() {
   return (
     <div className={`scroll-to-top ${isVisible ? 'visible' : ''}`} onClick={scrollToTop}>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-        <path fillRule="evenodd" d="M12 14.25a.75.75 0 01-.75-.75V2.862l-5.47 5.47a.75.75 0 11-1.06-1.06l6.75-6.75a.75.75 0 011.06 0l6.75 6.75a.75.75 0 11-1.06 1.06l-5.47-5.47V13.5a.75.75 0 01-.75.75z" clipRule="evenodd" />      </svg>
+        <path fillRule="evenodd" d="M12 18.75a.75.75 0 01-.75-.75V7.362l-5.47 5.47a.75.75 0 11-1.06-1.06l6.75-6.75a.75.75 0 011.06 0l6.75 6.75a.75.75 0 11-1.06 1.06l-5.47-5.47V18a.75.75 0 01-.75.75z" clipRule="evenodd" />     </svg>
     </div>
   );
 }
--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -20,15 +20,16 @@ function TopPage() {
         <div className='toppage-content-block '>
           <RecentArticles limit={2} />
         </div>
+
+        <div className='toppage-content-block '>
+          <ProfileContent />
+        </div>
         <div className='toppage-content-block '>
           <div className='git-glass'>
             <h2 >Githubの草</h2>
             <img src="https://github-contributions-api.deno.dev/yuich-r.svg" />
           </div>
 
-        </div>
-        <div className='toppage-content-block '>
-          <ProfileContent />
         </div>
       </div>
       <ScrollToTopButton />
